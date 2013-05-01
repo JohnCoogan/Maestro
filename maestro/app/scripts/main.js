fff8dd5d7742cb24bee40d347bb787367636f906
John Coogan <coogan.johna@gmail.com>
2013-05-01 15:14:25 -0400

    merged yeoman project with vexflow stuff

--- maestro/app/scripts/main.js
+++ maestro/app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+
+  function render_vexflow(element_id, data, render_options) {
+    var element = document.getElementById(element_id);
+    // Add the note.
+    var json = new Vex.Flow.JSON(data);
+    json.render(element, render_options);
+  }
+
+  function noteCtrl($scope) {
+    $scope.customChord = '["C", "E", "G", "Bb"]';
+    $scope.render = render_vexflow;
+    $scope.redraw = function(element_id) {
+      $scope.render(element_id, angular.fromJson($scope.customChord));
+    }
+
+    $scope.render('simple-chord', angular.fromJson($scope.customChord));
+  }
